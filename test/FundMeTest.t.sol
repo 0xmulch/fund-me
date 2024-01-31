37975d76af6256c0f5cc386055a171de2e1604b3
0xMulch <0xMulch@gmail.com>
2024-01-30 18:13:11 -0600

    .

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -15,6 +15,13 @@ contract FundMeTest is Test {
 
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+        //5e18 ==> 5 * 10 ** 18
+    }
+
+    function testOwnerIsMessageSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+        //we don't use msg.sender on the right, because message.sender is our account
+        //and the deployer of FundMe was the Test Contract, not actually msg.sender (me)
     }
 
 }
