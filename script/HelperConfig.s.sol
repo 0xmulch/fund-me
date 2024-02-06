862a2fa558ab0ca6e81c8ec7e675e6a087488f45
0xMulch <0xMulch@gmail.com>
2024-02-05 20:30:00 -0600

    Added Mainnet forkability

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,19 +2,45 @@
 
 pragma solidity 0.8.19;
 
-import {Script} from "../lib/forge-std/src";
+import {Script} from "forge-std/Script.sol";
 
 
 contract HelperConfig {
 
+    NetworkConfig public activeNetwork;
+
     struct NetworkConfig {
-        address priceFeed;
+        address priceFeed; // ETH/USD Feed
     }
-    
-    function getSepoliaEthConfig() {
-        NetworkConfig sepoliaConfig = NetworkConfig({
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetwork = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetwork = getMainnetEthConfig();
+        } else {
+            activeNetwork = getAnvilEthConfig();
+        }
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+
+        return mainnetConfig;
+    }   
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
+
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+
     }
 
 
